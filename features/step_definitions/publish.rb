0966c7eb5f883fa1ac1ef2f1859867dd5f932627
Sergey Novikov <snov@snov.me>
2016-05-17 14:04:29 +0200

    Add sleep

--- features/step_definitions/publish.rb
+++ features/step_definitions/publish.rb
@@ -18,3 +18,7 @@ end
 Then(/^I should not see "([^"]*)" in file "([^"]*)"$/) do |message, file|
   expect(file_contains_string(file, message)).to be_falsey
 end
+
+Then(/^I sleep for (\d+) seconds$/) do |n|
+  sleep(n.to_i)
+end

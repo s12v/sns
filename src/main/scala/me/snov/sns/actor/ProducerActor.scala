c008f1569bcfe205171f71145471c497c17ca411
Andreas Kohn <andreas.kohn@gmail.com>
2017-05-22 11:59:00 +0200

    Use the 'Oneway' trait instead of duplicating that logic

--- src/main/scala/me/snov/sns/actor/ProducerActor.scala
+++ src/main/scala/me/snov/sns/actor/ProducerActor.scala
@@ -1,13 +1,12 @@
 package me.snov.sns.actor
 
 import akka.actor.{Actor, ActorLogging, Props}
-import akka.camel.Producer
+import akka.camel.Oneway
 
 object ProducerActor {
   def props(endpoint: String) = Props(classOf[ProducerActor], endpoint)
 }
 
-class ProducerActor(endpoint: String) extends Actor with Producer with ActorLogging {
+class ProducerActor(endpoint: String) extends Actor with Oneway with ActorLogging {
   def endpointUri = endpoint
-  override def oneway = true
 }

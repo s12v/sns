a0b95eab640748defe537ebeffca9df4b7ee6559
jononu <jon.radon@onu1.com>
2018-03-13 15:30:43 -0400

    Always use POST for HTTP endpoints (#41)

--- src/main/scala/me/snov/sns/actor/ProducerActor.scala
+++ src/main/scala/me/snov/sns/actor/ProducerActor.scala
@@ -1,11 +1,11 @@
 package me.snov.sns.actor
 
 import akka.actor.{Actor, ActorLogging, Props}
-import akka.camel.{CamelMessage,Oneway}
-
+import akka.camel.{CamelMessage, Oneway}
 import spray.json._
-
 import me.snov.sns.model.Message
+import org.apache.camel.Exchange
+import org.apache.camel.component.http.HttpMethods
 
 object ProducerActor {
   def props(endpoint: String, subscriptionArn: String, topicArn: String) = Props(classOf[ProducerActor], endpoint, subscriptionArn, topicArn)
@@ -19,6 +19,7 @@ class ProducerActor(endpoint: String, subscriptionArn: String, topicArn: String)
       new CamelMessage(snsMsg.toJson.toString, Map(
 
         CamelMessage.MessageExchangeId -> snsMsg.uuid,
+        Exchange.HTTP_METHOD -> HttpMethods.POST,
         "x-amz-sns-message-type" -> "Notification",
         "x-amz-sns-message-id" -> snsMsg.uuid,
         "x-amz-sns-subscription-arn" -> subscriptionArn,
